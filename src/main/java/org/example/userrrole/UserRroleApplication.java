eddc264195c18ef8b53ec01bd5ad0f1f925996c4
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2024-10-21 15:14:24 +0500

    Initial commit

---
+++ src/main/java/org/example/userrrole/UserRroleApplication.java
@@ -0,0 +1,13 @@
+package org.example.userrrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRroleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRroleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userrrole/UserRroleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userrrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRroleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
